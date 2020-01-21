95af8c83f95f02a9573d1de4f5fcf14163f18764
moises <elnaufrago2009@gmail.com>
2020-01-20 22:18:52 -0500

    base

--- public/js/app.js
+++ public/js/app.js
@@ -55937,8 +55937,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/moises/sites/php/invoices/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/moises/sites/php/invoices/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\invoices\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\invoices\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
